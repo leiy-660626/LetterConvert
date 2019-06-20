dc635eb3f55746456d2ee48b4c556ecfbf19859a
leiyang <leiyang-it@bestpay.com.cn>
2019-06-20 16:07:05 +0800

    commint a little test

---
+++ convert/src/main/java/LetterConvert.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+/**
+
+ * converting the digits from 0 to 99 into letters
+ * @author leiy
+ * @Time 2019-06-20 10:49:01
+ *
+ */
+public class LetterConvert {
+
+    public static void main(String[] args){
+        int[] d = {3, 22};
+        ArrayList<String> resultList = new LetterConvert().convertLetter(d);
+        System.out.println(resultList);
+
+    }
+
+    /***
+     * main funtion to convert digit into letters
+     * @param inputData
+     * @return rstList
+     */
+    public  ArrayList<String> convertLetter(int[] inputData) {
+
+        char[][] charSet = new char[][]{
+                {},{},
+                {'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'},
+                {'j', 'k', 'l'}, {'m', 'n', 'o'}, {'p', 'q', 'r', 's'},
+                {'t', 'u', 'v'}, {'w', 'x', 'y', 'z'}
+        };
+
+        ArrayList<String> rstList = new ArrayList<String>();
+        if(null == inputData){
+            return rstList;
+        }
+
+        int length = inputData.length;
+        for (int i = 0; i < length; i++) {
+            if(inputData[i] < 0 || inputData[i] > 99){
+                System.out.println(inputData[i] + " is out of boundaries, please make sure the digitle is in[0, 99]" );
+                continue;
+            }
+            mixData(rstList, inputData[i], charSet);
+        }
+       return removeDuplication(rstList);
+
+    }
+
+    /***
+     * mix the current number with result list
+     * @param rstList the result List
+     * @param index each of the input array data
+     * @param charSet original charset
+     */
+    private  void mixData(ArrayList<String> rstList, int index, char[][] charSet){
+        ArrayList<String> l=new ArrayList<String>();
+        ArrayList<String> tt=new ArrayList<String>(rstList);
+        rstList.clear();
+
+        if(index < 10) {
+            for (int i = 0; i < charSet[index].length; i++) {
+                l.add(String.valueOf(charSet[index][i]));
+            }
+        }else if(index < 99){
+            int sw = index/10;
+            int gw = index%10;
+
+            for (int i = 0; i < charSet[sw].length; i++) {
+                l.add(String.valueOf(charSet[sw][i]));
+            }
+
+            for (int i = 0; i < charSet[gw].length; i++) {
+                l.add(String.valueOf(charSet[gw][i]));
+            }
+        }
+        if(tt.size()==0){
+            for(int i=0;i<l.size();i++){
+                rstList.add(l.get(i));
+            }
+        }else{
+            for(int i=0;i<tt.size();i++){
+                for(int j=0;j<l.size();j++){
+                    rstList.add(tt.get(i)+l.get(j));
+                }
+            }
+        }
+    }
+
+    /***
+     *
+     * @param listData original
+     * @return result data which is not repeat
+     */
+    private ArrayList<String> removeDuplication(ArrayList<String> listData){
+
+        Set<String> middleHashSet = new HashSet<String>(listData);
+        listData.clear();
+        listData = new ArrayList<String>(middleHashSet);
+        return listData;
+    }
+
+}
+
---
+++ convert/src/test/java/LetterConvertTest.java
@@ -0,0 +1,67 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class LetterConvertTest {
+    @Test
+    public void testConvert1(){
+        int [] inputData = {0, 2, 3};
+
+        LetterConvert lc = new LetterConvert();
+        ArrayList<String> resultList = lc.convertLetter(inputData);
+        Collections.sort(resultList);
+
+        ArrayList<String> assertResult = new ArrayList<String>();
+        assertResult.add("ad");assertResult.add("ae");assertResult.add("af");
+        assertResult.add("bd");assertResult.add("be");assertResult.add("bf");
+        assertResult.add("cd");assertResult.add("ce");assertResult.add("cf");
+        Collections.sort(assertResult);
+        assertEquals(assertResult, resultList);
+    }
+
+    @Test
+    public void testConvert2(){
+        int [] inputData = {9};
+
+        LetterConvert lc = new LetterConvert();
+        ArrayList<String> resultList = lc.convertLetter(inputData);
+        Collections.sort(resultList);
+
+        ArrayList<String> assertResult = new ArrayList<String>();
+        assertResult.add("w");assertResult.add("x");assertResult.add("y");
+        assertResult.add("z");
+        Collections.sort(assertResult);
+
+        assertEquals(assertResult, resultList);
+    }
+
+    @Test
+    public void testConvert3(){
+        int [] inputData = {-1, 100};
+
+        LetterConvert lc = new LetterConvert();
+        ArrayList<String> resultList = lc.convertLetter(inputData);
+
+        assertEquals(0, resultList.size());
+    }
+
+    @Test
+    public void testConvert4(){
+        int [] inputData = { 22, 3};
+
+        LetterConvert lc = new LetterConvert();
+        ArrayList<String> resultList = lc.convertLetter(inputData);
+        Collections.sort(resultList);
+
+        ArrayList<String> assertResult = new ArrayList<String>();
+        assertResult.add("ad");assertResult.add("ae");assertResult.add("af");
+        assertResult.add("bd");assertResult.add("be");assertResult.add("bf");
+        assertResult.add("cd");assertResult.add("ce");assertResult.add("cf");
+        Collections.sort(assertResult);
+
+        assertEquals(assertResult, resultList);
+    }
+
+}
